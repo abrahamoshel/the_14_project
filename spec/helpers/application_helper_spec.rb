834a1cee9c04e4f34ad87e90b59801350c10a988
Abraham Oshel and Jesse Swartzendruber <dev+abrahamoshel+arrogantyeti101@gmail.com>
2014-03-23 17:26:16 -0500

    Get specs to pass with a little voodo

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,17 +1,32 @@
 require 'spec_helper'
 
-describe ApplicationHelper do
-  include Devise::TestHelpers
+module FakeDeviseModule
+  ANSWER = false
+  def user_signed_in?
+    ANSWER
+  end
+end
 
-  it "gives the SIGN IN link if a user is NOT signed in" do
-    helper.stub(:current_user) { nil }
-    helper.stub(:user_signed_in?) { false }
-    expect(session_link_helper).to eq(link_to "Login", new_user_session_path)
+describe ApplicationHelper do
+  include FakeDeviseModule
+
+  before :each do
+    FakeDeviseModule::ANSWER = answer
   end
 
-  it "gives the SIGN OUT link if a user is signed in" do
-    helper.stub(:current_user) { User.new }
-    helper.stub(:user_signed_in?) { true }
-    expect(session_link_helper).to eq(link_to "Logout", destroy_user_session_path, method: :delete)
+  context "User is NOT signed in" do
+    let(:answer) { false }
+
+    it "gives the SIGN IN link" do
+      expect(session_link_helper).to eq(link_to "Login", new_user_session_path)
+    end
+  end
+
+  context "User IS signed in" do
+    let(:answer) { true }
+
+    it "gives the SIGN OUT link" do
+      expect(session_link_helper).to eq(link_to "Logout", destroy_user_session_path, method: :delete)
+    end
   end
 end
